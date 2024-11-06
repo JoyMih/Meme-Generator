b08764f9bc8a1ee6e0981c592c9febdbb01b0c5a
Joy H <joyhwangbvt2023@gmail.com>
2024-11-05 20:27:09 -0800

    adjusted css for meme component grid for better accessibility

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -3,8 +3,12 @@ function Meme() {
         <main>
             <form className="form">
                 {/* <div className="text-areas"> */}
+                <label className="form-label"> Top Text
                     <input type="text" placeholder="Top text" className="form-input" />
+                </label>
+                <label className="form-label"> Bottom Text
                     <input type="text" placeholder="Bottom Text" className="form-input" />
+                </label>
                 {/* </div> */}
                 <button className="form-btn">
                     Get a new meme image 🖼

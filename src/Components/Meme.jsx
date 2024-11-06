d4b62fc0a6bbd08d548ee7b9a5cf18f1b2b34ca6
Joy H <joyhwangbvt2023@gmail.com>
2024-11-05 20:30:23 -0800

    updated google font styling link in index.html file

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -4,10 +4,10 @@ function Meme() {
             <form className="form">
                 {/* <div className="text-areas"> */}
                 <label className="form-label"> Top Text
-                    <input type="text" placeholder="Top text" className="form-input" />
+                    <input type="text" placeholder="Shut up" className="form-input" />
                 </label>
                 <label className="form-label"> Bottom Text
-                    <input type="text" placeholder="Bottom Text" className="form-input" />
+                    <input type="text" placeholder="and take my money" className="form-input" />
                 </label>
                 {/* </div> */}
                 <button className="form-btn">

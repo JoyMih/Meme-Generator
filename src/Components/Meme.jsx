8f50b3b0024d1887f73166c1ca2043949ea86915
Joy H <joyhwangbvt2023@gmail.com>
2024-11-05 20:15:02 -0800

    adjusted css grid for Meme/main component

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -2,10 +2,10 @@ function Meme() {
     return (
         <main>
             <form className="form">
-                <div className="text-areas">
+                {/* <div className="text-areas"> */}
                     <input type="text" placeholder="Top text" className="form-input" />
                     <input type="text" placeholder="Bottom Text" className="form-input" />
-                </div>
+                {/* </div> */}
                 <button className="form-btn">
                     Get a new meme image 🖼
                 </button>

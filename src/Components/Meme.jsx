2f48e38af83cfe95861842b8a690f7953d093ee5
Joy H <joyhwangbvt2023@gmail.com>
2024-11-23 22:14:24 -0800

    Added data fetching via useEffect to get latest data from the meme API, disconnected the static imported data

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,34 +1,48 @@
-import memesData from "../memesData.jsx"
+// import memesData from "../secrets/memesData.jsx"
 import React from "react"
 
+/* Meme component */
 function Meme() {
-    const [memeImage, setMemeImage] = React.useState({ // First State Variable
+    const [memeImage, setMemeImage] = React.useState({ // First State Variable to be instantiated
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg",
         altText: "One Does Not Simply"
     });
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData); // Second State Variable
+    const [allMemeImages, setAllMemeImages] = React.useState([]); // Second State Variable
 
+    /* Fetch data from API: Batch retrieve the array of all memes upon first render using useEffect */
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes") // API endpoint we request data from
+        .then((response) => response.json()) // .json method on the response
+        .then((data) => setAllMemeImages(data.data.memes)) // Setting the received data/saving it to the allMemeImages state
+    }, []); // Dependency array empty, as we only want a single fetch request upon first render
+    
+    /* Checking the data we retrieved from the fetch request*/
+    React.useEffect(() => {
+        console.log(allMemeImages); // Log only when `allMemeImages` changes
+    }, [allMemeImages]);
+
+    /* Function to Get Meme */
     function getRandomImage() {
-        const memesArray = allMemeImages.data.memes;
-        const randomNumber = Math.floor(Math.random() * (memesArray.length + 1));
-        const memeUrl = memesArray[randomNumber].url;
-        const memeAltText = memesArray[randomNumber].name;
-        console.log(memeAltText);
+        // old line of code that utilized imported static data--> const memesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * (allMemeImages.length));
+        const memeUrl = allMemeImages[randomNumber].url;
+        const memeAltText = allMemeImages[randomNumber].name;
 
         setMemeImage((prevMemeImage) => ({ // Surrounding the return with parentheses in order to use implicit return
             ...prevMemeImage, // Pulling all of the properties of the previous meme
             randomImage: memeUrl, // Altering the random image property
-            altText: memeAltText // Alternate text
+            altText: memeAltText // Alternate text for each image
         }))
     }
 
+    /* Event handler for setting new image upon a change occurring with an HTML element */
     function handleChange( event ) { // Passing in the event object
         const { name, value } = event.target // unpacking the name and value property from the event object
         setMemeImage((prevMemeImage) => ({
             ...prevMemeImage,
-            [ name ]: value
+            [ name ]: value // targeting and updating the passed in property and value
         }))
     }
 
@@ -38,7 +52,7 @@ function Meme() {
                 <label className="form-label"> Top Text
                     <input
                         type="text"
-                        placeholder="Shut up"
+                        placeholder="Top Text"
                         className="form-input"
                         name="topText" // Name property allows for us to watch for changes
                         value={ memeImage.topText } // Value property allows for controlling of this component
@@ -48,7 +62,7 @@ function Meme() {
                 <label className="form-label"> Bottom Text
                     <input
                         type="text"
-                        placeholder="and take my money"
+                        placeholder="Bottom Text"
                         className="form-input"
                         name="bottomText" // Name property allows for us to watch for changes
                         value={ memeImage.bottomText } //  Value property allows for controlling of this component
@@ -62,7 +76,7 @@ function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img className="image-format" src={memeImage.randomImage} alt="" />
+                <img className="image-format" src={memeImage.randomImage} alt={memeImage.altText} />
                 <h2 className="meme--text top">{memeImage.topText}</h2>
                 <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
             </div>
